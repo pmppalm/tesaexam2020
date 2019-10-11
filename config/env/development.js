26734b65884c8c21264c52284adb6fc43753b558
Wongsathorn Jaisin <palm_paper@hotmail.com>
2019-10-11 11:38:29 +0700

    edit db

---
+++ config/env/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+    debug: true,
+    mongoUri: 'mongodb://localhost/tgr14',
+    sessionSecret: 'dev_secret_key'
+};
---
+++ config/env/production.js
@@ -0,0 +1,5 @@
+module.exports = {
+    debug: false,
+    mongoUri: 'mongodb://localhost//tgr14',
+    sessionSecret: 'prod_secret_key'
+};
