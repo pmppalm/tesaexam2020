722afedbbfcbfddc9d36d32e5f4be6632909e7f8
pmppalm <palm_paper@hotmil.com>
2019-10-12 02:13:03 +0700

    change DBLocalMLAB

--- config/mongoose.js
+++ config/mongoose.js
@@ -12,7 +12,7 @@ const options = {
   };
 module.exports = function () {
     mongoose.set('debug', config.debug);
-    var db = mongoose.connect('mongodb://localhost/tgr14',options).then(
+    var db = mongoose.connect(config.mongoUri,options).then(
         ()=>{
           console.log("connected to mongoDB")},
        (err)=>{
