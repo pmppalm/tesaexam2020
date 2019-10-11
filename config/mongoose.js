913458898ae14f512e8eaeab1dc15b11cd00d446
Phatin Assawaratanawarang <43286845+pmppalm@users.noreply.github.com>
2019-10-11 14:41:25 +0700

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 
 module.exports = function () {
     mongoose.set('debug', config.debug);
-   var db = mongoose.connect(config.mongoUri);
+   //var db = mongoose.connect(config.mongoUri);
    //var db = mongoose.connect('mongodb://tgr13:silpakorn13@ds235775.mlab.com:35775/heroku_jm7p5fsc');
    //var db = mongoose.connect('mongodb://heroku_wjq5hw7f:qa1p9074squ4fkeo5utqd26tr5@ds153593.mlab.com:53593/heroku_wjq5hw7f');
-   //var db = mongoose.connect('mongodb://heroku_jvp8kncs:tloup834159i629f624onibgbn@ds117101.mlab.com:17101/heroku_jvp8kncs',{ useNewUrlParser: true });
+   var db = mongoose.connect('mongodb://heroku_jvp8kncs:tloup834159i629f624onibgbn@ds117101.mlab.com:17101/heroku_jvp8kncs',{ useNewUrlParser: true });
 
     //var db = mongoose.connect('mongodb://localhost/tgr14');
 
