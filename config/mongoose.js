566e6916dc169e5bb7d75a2e9ab96fe82efd6934
Wongsathorn Jaisin <palm_paper@hotmail.com>
2019-10-13 23:25:21 +0700

    change DB2

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,39 +2,26 @@ var config = require('./config');
 var mongoose = require('mongoose');
 
 const options = {
-    useNewUrlParser: true,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: 100, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0
-  };
+  useNewUrlParser: true,
+  autoIndex: false, // Don't build indexes
+  reconnectTries: 100, // Never stop trying to reconnect
+  reconnectInterval: 500, // Reconnect every 500ms
+  poolSize: 10, // Maintain up to 10 socket connections
+  // If not connected, return errors immediately rather than waiting for reconnect
+  bufferMaxEntries: 0
+};
 module.exports = function () {
-    mongoose.set('debug', config.debug);
-    var db = mongoose.connect(config.mongoUri,options).then(
-        ()=>{
-          console.log("connected to mongoDB")},
-       (err)=>{
-           console.log("err",err);
-      }
-      );
-    /*var db = mongoose.connect('mongodb://heroku_jvp8kncs:tloup834159i629f624onibgbn@ds117101.mlab.com:17101/heroku_jvp8kncs',{ useNewUrlParser: true,
-    autoIndex: false, // Don't build indexes
-    reconnectTries: 100, // Never stop trying to reconnect
-    reconnectInterval: 500, // Reconnect every 500ms
-    poolSize: 10, // Maintain up to 10 socket connections
-    // If not connected, return errors immediately rather than waiting for reconnect
-    bufferMaxEntries: 0 }).then(
-        ()=>{
-          console.log("connected to mongoDB")},
-       (err)=>{
-           console.log("err = ",err);
-       });
-    
-*/
-    require('../app/models/user.model');
-    require('../app/models/exam.model');
-    return db;
+  mongoose.set('debug', config.debug);
+  var db = mongoose.connect('mongodb://localhost/tgr14', options).then(
+    () => {
+      console.log("connected to mongoDB")
+    },
+    (err) => {
+      console.log("err", err);
+    }
+  );
+  require('../app/models/user.model');
+  require('../app/models/exam.model');
+  return db;
 }
 
